3831913ec8cca53b5df1db89c322966e9716a08a
Slimey <blastslimeyt@gmail.com>
2025-01-13 20:13:08 +0100

    Restructure ConnectionList to JSON format, add OfflineItems and PrintHammerCollision

--- Config.cs
+++ Config.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Linq;
 using BepInEx.Configuration;
 
 public struct ConnectionInfo {
@@ -21,18 +23,30 @@ public class ConfigHandler {
 
     public static ConfigEntry<string> ConnectionsList;
     public static ConfigEntry<int> ActiveSlot;
+    public static ConfigEntry<string> OfflineItems;
+    public static ConfigEntry<bool> PrintHammerCollision;
 
     public static void InitConfig(ConfigFile Config) {
         ConnectionsList = Config.Bind(
             "General", "ConnectionList", 
-            "SlotName,,,archipelago.gg:38281,,,password;;;Player1,,,localhost:38281,,,;;;AnotherPlayer,,,your.APServer.com,,,password1234", 
+            "[{'slot':'SlotName','addressPort':'archipelago.gg:38281','password':'psswrd123'},"+
+            "{'slot':'AnotherPlayer','addressPort':'your.APServer.com','password':'top.scrt'},"+
+            "{'slot':'Player1','addressPort':'localhost:38281'}]", 
             "A list of connection details, so you don't have to re-enter them every time you play another slot. " + 
-            "Every entry consists of the slot name, server address:port, and password. " + 
-            "See examples for how to correctly format your entries."
+            "Has to be formatted as a JSON string."
         );
         ActiveSlot = Config.Bind(
             "General", "ActiveSlot", -1, 
             "The ConnectionList entry to use for connecting to a sever. Begins with 0 as the first entry. Use -1 to disable connecting."
+        );
+        OfflineItems = Config.Bind(
+            "General", "OfflineItems",
+            "{'Gravity Reduction':2,'Goal Height Reduction':0,'Wind Trap':0}",
+            "Define a set inventory that should be used if the game is not connected to any Archipelago server."
+        );
+        PrintHammerCollision = Config.Bind(
+            "General", "PrintHammerCollision", false, 
+            "Print every collision of the hammer with anything to the console. For debug purposes."
         );
     }
 
--- Connection.cs
+++ Connection.cs
@@ -1,49 +1,47 @@
 
 using System;
-using System.Linq;
 using Archipelago.MultiClient.Net;
 using Archipelago.MultiClient.Net.Enums;
 using Archipelago.MultiClient.Net.Models;
 using BepInEx.Logging;
+using Newtonsoft.Json.Linq;
 
 public class ConnectionHandler {
 
     public static ArchipelagoSession Session;
     public static LoginSuccessful Success;
     public static ManualLogSource Logger;
-    public static int GravityReductions = 0;
+    public static int GravityReductions = 2;
     public static int GoalHeightReductions = 0;
     public static int WindTraps = 0;
 
-    public static void Connect(ManualLogSource logger, Action updateGravity) {
+    public static void Connect(ManualLogSource logger, Action<bool> updateGravity) {
 
         Logger = logger;
+        ProcessOfflineList();
 
-        string[] list = ConfigHandler.ConnectionsList.Value.Split([";;;"], StringSplitOptions.None);
+        JArray list = JArray.Parse(ConfigHandler.ConnectionsList.Value);
         int active = ConfigHandler.ActiveSlot.Value;
         if (active < -1) {
             Logger.LogWarning($"Illegal ActiveSlot value: {active}");
             return;
-        } else if (active >= list.Length) {
+        } else if (active >= list.Count) {
             Logger.LogWarning($"ActiveSlot value out of range: {active}");
             return;
         } else if (active == -1) {
             Logger.LogInfo("Connecting disabled as per config");
             return;
         }
-        string[] activeList = list[active].Split([",,,"], StringSplitOptions.None);
-        if (activeList.Length < 2) {
-            Logger.LogWarning($"Chosen slot contains not enough information");
-            return;
-        } else if (activeList.Length == 2) {
-            activeList.Append("");
-        }
+        JToken activeList = list[active];
+        string activeAddressPort = activeList.Value<string>("addressPort") ?? "localhost:38281";
+        string activeSlot = activeList.Value<string>("slot") ?? "Player1";
+        string activePassword = activeList.Value<string>("password") ?? "";
 
-        Session = ArchipelagoSessionFactory.CreateSession(activeList[1]);
+        Session = ArchipelagoSessionFactory.CreateSession(activeAddressPort);
         LoginResult result;
         try {
             result = Session.TryConnectAndLogin(
-                "Getting Over It", activeList[0], ItemsHandlingFlags.AllItems, password: activeList[2]
+                "Getting Over It", activeSlot, ItemsHandlingFlags.AllItems, password: activePassword
             );
         } catch (Exception e) {
             result = new LoginFailure(e.GetBaseException().Message);
@@ -51,13 +49,14 @@ public class ConnectionHandler {
 
         if (!result.Successful) {
             LoginFailure failure = (LoginFailure)result;
-            string errorMessage = $"Failed to Connect to {activeList[1]} as {activeList[0]}:";
+            string errorMessage = $"Failed to Connect to {activeAddressPort} as {activeSlot}:";
             foreach (string error in failure.Errors) {
                 errorMessage += $"\n    {error}";
             }
             foreach (ConnectionRefusedError error in failure.ErrorCodes) {
                 errorMessage += $"\n    {error}";
             }
+            logger.LogError(errorMessage);
         } else {
             Success = (LoginSuccessful)result;
             logger.LogInfo("Connection successful");
@@ -72,22 +71,40 @@ public class ConnectionHandler {
                     else if (item.ItemName.Equals("Wind Trap")) WindTraps++;
                 }
                 if (GravityReductions > 4) GravityReductions = 4;
-                if (GoalHeightReductions > 4) GoalHeightReductions = 4;
+                if (GoalHeightReductions > 6) GoalHeightReductions = 6;
                 if (WindTraps > 6) WindTraps = 6;
                 logger.LogInfo(
                     $"Gravity Reductions: {GravityReductions}, Goal Height Reductions: {GoalHeightReductions}, Wind Traps: {WindTraps}"
                 );
-                updateGravity();
+                updateGravity(false);
             };
         }
 
     }
 
+    public static void ProcessOfflineList() {
+        
+        Logger.LogInfo("Processing offline inventory...");
+        JObject inv = JObject.Parse(ConfigHandler.OfflineItems.Value);
+        GravityReductions = inv.Value<int?>("Gravity Reduction") ?? 2;
+        GoalHeightReductions = inv.Value<int?>("Goal Height Reduction") ?? 2;
+        WindTraps = inv.Value<int?>("Wind Trap") ?? 2;
+        if (GravityReductions > 4) GravityReductions = 4;
+        if (GoalHeightReductions > 6) GoalHeightReductions = 6;
+        if (WindTraps > 6) WindTraps = 6;
+        Logger.LogInfo(
+            $"Gravity Reductions: {GravityReductions}, Goal Height Reductions: {GoalHeightReductions}, Wind Traps: {WindTraps}"
+        );
+
+    }
+
     public static void CheckLocation(string name) {
+        if (Success == null) return;
         CheckLocation(Session.Locations.GetLocationIdFromName("Getting Over It", name), name);
     }
 
     public static void CheckLocation(long id, string name = null) {
+        if (Success == null) return;
         Logger.LogInfo($"Checking location {(name == null ? id : name)}");
         Session.Locations.CompleteLocationChecks([id]);
     }
